62e63329d914b8df7e507b3b5d4368f40da824d8
Adriano-Brito77 <102468294+Adriano-Brito77@users.noreply.github.com>
2024-12-03 21:27:20 -0300

    card_sales

--- frontend/src/components/pages/Salelist.jsx
+++ frontend/src/components/pages/Salelist.jsx
@@ -1,60 +1,70 @@
-import style from "./SaleList.module.css"
+import style from "./SaleList.module.css";
 
 const Salelist = () => {
-
-    const sale = [
-		{
-			numsales: 1,
-			totalvalue:30,
-			receive:0,
-			change:0,
-			username:"murilo"
-		},{
-			numsales: 1,
-			totalvalue:30,
-			receive:0,
-			change:0,
-			username:"murilo"
-		}
-	]
-    const saleItens = [
-          
-            {
-        "description":"Arroz",
-        "barcode": 2080,
-        "unitprice": 5.5,
-        "unit":"UN",
-        "salebalance": 1
-        
-            }
-        ,
-            {
-        "name": "Coca 300ml",
-        "barcode": 350,
-        "unitprice": 5,
-        "unit": "ML",
-        "salebalance":1
-            }
-]
-
-
+  const sales = [
+    {
+      numsales: 1,
+      totalvalue: 30,
+      receive: 0,
+      change: 0,
+      username: "murilo",
+    },
+    {
+      numsales: 2,
+      totalvalue: 30,
+      receive: 0,
+      change: 0,
+      username: "murilo",
+    },
+  ];
+  const saleItens = {
+    numsales: 1,
+    items: [
+      {
+        description: "Arroz",
+        barcode: 2080,
+        unitprice: 5.5,
+        unit: "UN",
+        salebalance: 1,
+      },
+      {
+        name: "Coca 300ml",
+        barcode: 350,
+        unitprice: 5,
+        unit: "ML",
+        salebalance: 1,
+      },
+    ],
+  };
 
   return (
-    <div className={style.container}>
-        <div className={style.btn_sale}>
-            <button>
-              
-
-
-            </button>
-        </div>
-        <div>
+    <div className={style.container_sale_list}>
+      <div className={style.btn_sale}>
+        {sales.map((sale) => (
+          <button>
             <span>
-                TESTE
+              Numero da venda: <p>{sale.numsales}</p>
             </span>
-            </div>
-    </div>
-  )
-}
+            <span>
+              Vendedor: <p>{sale.username}</p>
+            </span>
+            <span>
+              Valor total: <p>{sale.totalvalue}</p>
+            </span>
+            <span>
+              Valor recebido: <p>{sale.receive}</p>
+            </span>
+            <span>
+              Troco: <p>{sale.change}</p>
+            </span>
+          </button>
+        ))}
 
-export default Salelist
+        {<span>{saleItens.items.map((itens) => itens.barcode)}</span>}
+      </div>
+      <div className={style.itens_list}></div>
+    </div>
+  );
+};
+
+export default Salelist;
